51d88330f8c0b502508c69b5b6cb1ac8e27d5879
pdeford <pdeford1@jhu.edu>
2016-09-26 21:49:04 -0400

    Add batch submission file

---
+++ scripts/batch_submission.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python 
+# 
+# Submit files to challenge
+# 
+doc = """Usage: make-submissions SUBMISSIONFILES..."""
+
+from docopt import docopt
+import synapseclient
+from synapseclient import File
+
+# 
+# Parse options
+# 
+opts = docopt(doc) 
+submission_files = opts['SUBMISSIONFILES']
+
+# 
+# Log in 
+# 
+print('Logging in.')
+syn = synapseclient.Synapse()
+syn.login() 
+
+# 
+# Our project 
+# 
+print('Getting project.')
+project = syn.get(7114158) 
+
+# 
+# Get the object representing the evaluation queue
+# 
+print('Getting evaluation queue.')
+# Ladder Round 
+evaluation = syn.getEvaluation(7071644)
+# Final Round
+# evaluation = syn.getEvaluation(7212779)
+
+
+for filename in submission_files: 
+    #
+    # Upload our file 
+    #
+    print('Uploading: {}'.format(filename)) 
+    upload_file = File(filename, description='Submission', parentId = project.id) 
+    upload_file = syn.store(upload_file)
+
+    #
+    # Submit the file to the evaluation queue
+    #
+    print('Submitting: {}'.format(filename))
+    submission = syn.submit(evaluation, upload_file, name='Submission', team='<insert your team name here>')
