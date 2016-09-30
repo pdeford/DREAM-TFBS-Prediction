d8d8fbe0d39935c9d112a99df801995a5abd3463
pdeford <pdeford1@jhu.edu>
2016-09-30 16:44:33 -0400

    Script for prepping overlap

--- scripts/prepare_DNase_overlap.sh
+++ scripts/prepare_DNase_overlap.sh
@@ -2,23 +2,26 @@
 
 source activate DREAM-TFBS
 
-data_dir='/cache/DREAM-tfbs-pred/'
+data_dir=$1
 out_dir=$data_dir'/out/'
 
 ls /cache/DREAM-tfbs-pred/DNase/conservative/ | cut -f2 -d '.' | while read cell
   do
-    bedtools intersect \
-      -loj -a $data_dir'/annotations/regions/train_regions.blacklistfiltered.bed'  \
-      -b  $data_dir'/DNase/conservative/DNASE.'$cell'.conservative.narrowPeak' \
-      > $out_dir$cell'_DNASE_intersect_train.out' &
-    
-    bedtools intersect \
-      -loj -a $data_dir'/annotations/regions/test_regions.blacklistfiltered.bed'  \
-      -b  $data_dir'/DNase/conservative/DNASE.'$cell'.conservative.narrowPeak' \
-      > $out_dir$cell'_DNASE_intersect_test.out' &
-    
-    bedtools intersect \
-      -loj -a $data_dir'/annotations/regions/ladder_regions.blacklistfiltered.bed'  \
-      -b  $data_dir'/DNase/conservative/DNASE.'$cell'.conservative.narrowPeak' \
-      > $out_dir$cell'_DNASE_intersect_ladder.out' &
+  	if [ ! -e $out_dir$cell'_DNASE_intersect_train.out' ]
+  	  then
+        bedtools intersect \
+          -loj -a $data_dir'/annotations/regions/train_regions.blacklistfiltered.bed'  \
+          -b  $data_dir'/DNase/conservative/DNASE.'$cell'.conservative.narrowPeak' \
+          > $out_dir$cell'_DNASE_intersect_train.out' &
+        
+        bedtools intersect \
+          -loj -a $data_dir'/annotations/regions/test_regions.blacklistfiltered.bed'  \
+          -b  $data_dir'/DNase/conservative/DNASE.'$cell'.conservative.narrowPeak' \
+          > $out_dir$cell'_DNASE_intersect_test.out' &
+        
+        bedtools intersect \
+          -loj -a $data_dir'/annotations/regions/ladder_regions.blacklistfiltered.bed'  \
+          -b  $data_dir'/DNase/conservative/DNASE.'$cell'.conservative.narrowPeak' \
+          > $out_dir$cell'_DNASE_intersect_ladder.out' &
+    fi
   done
