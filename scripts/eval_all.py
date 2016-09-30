8451c95d5b0444fb8350dc9cba907d3f1ad0893b
Peter DeFord <pdeford1@jhu.edu>
2016-09-30 15:26:09 -0400

    Improve file existence checks to included but un/compressed versions

--- scripts/eval_all.py
+++ scripts/eval_all.py
@@ -22,33 +22,37 @@ for line in f:
 		for cell in Round:
 			if cell not in all_cells: all_cells.append(cell)
 
-command = """if [ ! -e $out_dir'/%s.'$TF'.'$cell'.tab' ]
+command = """TF=%s; cell=%s; data_dir=%s; out_dir=%s;
+if [ ! -e $out_dir'/%s.'$TF'.'$cell'.tab.gz' ]
   then
-    TF=%s; cell=%s; data_dir=%s; out_dir=%s; python scripts/train_test_classifier.py \
-    $data_dir'/annotations/regions/%s' \
-    $TF \
-    $out_dir'/'$TF'_PWM.tsv' \
-    $out_dir'/'$TF'_StruM.tsv' \
-    $out_dir'/'$cell'_DNase.tsv' \
-    $out_dir'/RNA_vals.tsv' \
-    $cell \
-    $out_dir'/kmers.tsv' \
-    $out_dir \
-    $out_dir'/'$TF'_'*'_train.h5' \
-    > $out_dir'/%s.'$TF'.'$cell'.tab'
-fi;
-gzip $out_dir'/%s.'$TF'.'$cell'.tab'
+    if [ ! -e $out_dir'/%s.'$TF'.'$cell'.tab' ]
+      then 
+        python scripts/train_test_classifier.py \
+        $data_dir'/annotations/regions/%s' \
+        $TF \
+        $out_dir'/'$TF'_PWM.tsv' \
+        $out_dir'/'$TF'_StruM.tsv' \
+        $out_dir'/'$cell'_DNase.tsv' \
+        $out_dir'/RNA_vals.tsv' \
+        $cell \
+        $out_dir'/kmers.tsv' \
+        $out_dir \
+        $out_dir'/'$TF'_'*'_train.h5' \
+        > $out_dir'/%s.'$TF'.'$cell'.tab'
+  fi;
+  gzip $out_dir'/%s.'$TF'.'$cell'.tab'
+fi
 """
 
-for TF in TFs:
-	if sum( [1 for cell in TFs[TF]["F"] if cell != ""] ) == 0:
-		continue
-	for round in ["L","F"]:
+for round in ['L', 'F']:
+	for TF in sorted(TFs.keys()):
+		if sum( [1 for cell in TFs[TF]["F"] if cell != ""] ) == 0:
+			continue
 		if round == "L": region_file = "ladder_regions.blacklistfiltered.bed"
 		else: region_file = "test_regions.blacklistfiltered.bed"
 		for cell in TFs[TF][round]:
 			if cell != "":
-				subprocess.call(command % (round, TF, cell, data_dir, out_dir, region_file, round, round),
+				subprocess.call(command % (TF, cell, data_dir, out_dir, round, round, region_file, round, round),
 					shell=True)
 
 
